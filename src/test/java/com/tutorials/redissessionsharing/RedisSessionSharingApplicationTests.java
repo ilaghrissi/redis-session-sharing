7a28974736823ddf47dc6a51b066a73b0154f4d9
LAGHRISSI Imad <laghrissi.imad@gmail.com>
2022-04-21 16:37:19 +0200

    first commit

---
+++ src/test/java/com/tutorials/redissessionsharing/RedisSessionSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.redissessionsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSessionSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
